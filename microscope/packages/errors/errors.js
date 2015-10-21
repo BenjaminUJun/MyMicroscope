40b2ba6a43a493a6245e644a3207074ae9f6c8bc
Hugh <yujun_daxia@163.com>
2015-10-21 08:56:23 +0800

    notifications

--- microscope/packages/errors/errors.js
+++
@@ -1,8 +0,0 @@
-// Write your package code here!
-Errors = {
-	collection: new Mongo.Collection(null),
-	
-	throw: function(message) {
-		Errors.collection.insert({message: message, seen: false});
-	}
-};
